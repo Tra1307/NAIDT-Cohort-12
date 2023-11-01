96a60b04cdc40ba59dcf0b67c26728333d89698a
Tra1307 <127248563+Tra1307@users.noreply.github.com>
2023-11-01 09:30:14 +0100

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,33 @@
+'''
+Third Party Packages, PIP and Virtual Environments: 
+> Python Package Index (PyPi) and pip
+> Virtual Environments
+> installations
+'''
+
+"""
+# [The Python Package Index](https://pypi.org):
+is a repository of third-party Python packages that can be
+installed and used in Python projects.
+pip is a package manager for Python that allows you to easily install, manage, and uninstall
+packages from PyPi.
+With pip, you can install packages by running pip install <package_name> in the command line.
+pip automatically resolves dependencies and installs the required packages.
+"""
+"""
+# Virtual Environments(venv):
+A virtual environment is an isolated Python environment that allows you to install packages
+separately for different projects.
+Virtual environments help avoid conflicts between packages and ensure that each project has its
+own set of dependencies.
+"""
+
+"""
+installation:
+pip install virtualenv
+python -m venv .venv
+or
+python -m venv .venv
+pip install can now be used after activating the environment
+
+"""
